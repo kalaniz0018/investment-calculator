65c19f992d1e144dd0ea8ed447983b85d047562e
kalaniz0018 <karinaalaniz1994@gmail.com>
2024-04-05 17:58:20 -0300

    agregando comentario de uso de objeto

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import { calculateInvestmentResults } from "../util/investment";
 
 const Results = ({ input }) => {
-  const resultsData = calculateInvestmentResults(input)
+//Co el '+' adeñante convierto los valores de entrada en numeros antes de pasarlos a calculateInvestmentResults.
+  const userInput = {
+    initialInvestment: +input.initialInvestment,
+    annualInvestment: +input.annualInvestment,
+    expectedReturn: +input.expectedReturn,
+    duration: +input.duration,
+  };
+
+
+  const resultsData = calculateInvestmentResults(userInput)
   console.log(resultsData)
   return (
     <div>
-      <p>Results....</p>
+      <p>Results....</p>  
     </div>
   );
 };
