f1a1e58671d0bc30d35d3d7f5c20969f6a61ccb9
kalaniz0018 <karinaalaniz1994@gmail.com>
2024-04-05 18:19:45 -0300

    geenrando tabla para mostrar historial

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import UserInput from "./components/UserInput.jsx";
 import Results from "./components/Results.jsx";
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
+    initialInvestment: 15000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
