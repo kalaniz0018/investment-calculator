1907585e3a1746f9ece8e4ff9db94f897aca5221
kalaniz0018 <karinaalaniz1994@gmail.com>
2024-04-05 15:55:51 -0300

    creando nuevo componente y agregando explicacion  a la primera parte del flujo de informacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
 import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 function App() {
-  return <Header />;
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  //En la funcion handleChange toma el estado previo prevUserInput crea un nuevo objeto copinado todas
+  // las popiedades (...prevUserInput)
+  //y luego actualiza la propiedad especifica con inputIdentifier con  el nuevo valor guardado en newValue.
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
+    </>
+  );
 }
 
 export default App;
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,27 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-const UserInput = () => {
+const UserInput = ({ onChange, userInput }) => {
   return (
     <>
       <section id="user-input">
         <div className="input-group">
           <p>
             <label>Initial Investment</label>
-            <input type="number" required />
+            <input
+              type="number"
+              required
+              //value es valor actual del campo especifico, controla el input desde react y no desde el DOM directamente
+              /*controlar el estado desde React: El valor del input es gestionado por React y se actualiza a través de las 
+              actualizaciones de estado (setState o useState).Cualquier cambio en el valor del input provoca una actualización 
+              en el estado de React*/
+              /* Desde el Dom: El valor del input es manipulado directamente a través de JavaScript del lado del cliente 
+              sin involucrar el estado de React. No hay ninguna vinculación directa entre el valor del input y el estado de React y viceversa
+              Este enfoque puede ser útil en situaciones simples donde no se necesite un seguimiento del estado de los inputs */
+              value={userInput.initialInvestment}
+              onChange={(event) =>
+                onChange("initialInvestment", event.target.value)
+              }
+            />
           </p>
           <p>
             <label>Annual Investment</label>
-            <input type="number" required />
+            <input
+              type="number"
+              required
+              value={userInput.annualInvestment}
+              onChange={(event) =>
+                onChange("annualInvestment", event.target.value)
+              }
+            />
           </p>
         </div>
         <div className="input-group">
           <p>
             <label>Expected Return</label>
-            <input type="number" required />
+            <input
+              type="number"
+              required
+              value={userInput.expectedReturn}
+              onChange={(event) =>
+                onChange("expectedReturn", event.target.value)
+              }
+            />
           </p>
           <p>
             <label>Duration</label>
-            <input type="number" required />
+            <input
+              type="number"
+              required
+              value={userInput.duration}
+              onChange={(event) => onChange("duration", event.target.value)}
+            />
           </p>
         </div>
       </section>
